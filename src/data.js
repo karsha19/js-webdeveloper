4981aacda0d7939459af979278c8ed0737c4b185
Joao Silveira <joaofilipesilveira@gmail.com>
2023-03-24 16:42:51 +0000

    Configure React app for deployment to GitHub Pages

--- src/data.js
+++ src/data.js
@@ -47,7 +47,7 @@ export const WebDevelopment = [
          title: "RobotFriends",
          description: "Html, Css, React",
          img: 'https://images.unsplash.com/photo-1563396983906-b3795482a59a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80',
-         url: 'https://www.google.pt',
+         url: 'https://j-silveira.github.io/RobotFriends/',
      },
      {
          id: 4,
